64f31b6ec70a7129488a2640b8c9217fa2888da0
den <den25ru@gmail.com>
2023-05-29 12:29:59 +0200

    feam. new generetor

--- tests/something/test_something.py
+++ tests/something/test_something.py
@@ -1,5 +1,5 @@
 import pytest
-
+from src.generators.player_local import PlayerLocalization
 @pytest.mark.parametrize('status',[
     'ACTIVE',
     'BANNED',
@@ -17,7 +17,14 @@ def test_something(status, get_player_generator):
     'localize',
     'avatar'
 ])
-def test_something(delete_key, get_player_generator):
+def test_something1(delete_key, get_player_generator):
     object_to_send = get_player_generator.build()
     del object_to_send[delete_key]
     print(object_to_send)
+
+
+def something2(get_player_generator):
+    object_to_send = get_player_generator.update_inner_generator('localize', {PlayerLocalization('fr_FR')}).build()
+    print(object_to_send)
+
+
